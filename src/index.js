e5ec77e6f8f95898eea3d18abb4c4d1ec60e456c
Harun <harun@codex.works>
2022-02-03 11:59:16 +0700

    Feature: Add pwa and service worker

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { ApolloProvider } from "@apollo/client";
 
 import App from './routes'
 import client from './services/client'
+import * as serviceWorker from './utilities/serviceWorker'
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -19,3 +20,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
